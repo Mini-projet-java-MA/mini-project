f118f1de083ba47be13417053fa79513c1e29354
aaron <aaron@LAPTOP-JFVNBT5Q>
2020-04-27 13:40:10 +0300

    modiff

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -4,9 +4,20 @@ import primitives.*;
 
 import static primitives.Util.*;
 
+/**
+ * the cylinder the tube with height so he exsqlty like tube and he have one more param height
+ *
+ */
 public class Cylinder extends Tube {
     private double _height;
 
+    /**  this is the basic constructor for a tube :
+     * it receive ray and raduis,
+     * @param ray -the ray
+     * @param radius- the raduis
+     * @param height- the height of the (cylinder he limited with height)
+     * @throws if the radius equal on small to zero so we don't have a tube so his return IllegalArgumentException
+     */
     public Cylinder(Ray ray, double radius, double height) {
         super(ray, radius);
         if (height <= 0)
@@ -14,6 +25,10 @@ public class Cylinder extends Tube {
         this._height = height;
     }
 
+    /**
+     * simple function get
+     * @return the ray
+     */
     public double getHeight() {
         return _height;
     }
--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,27 +1,50 @@
 package geometries;
 
-import primitives.*;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
 
-
+/**
+ * tube class for represents tube tube define witch a radius and ray
+ *
+ * @author aaron
+ */
 public class Tube extends RadialGeometry {
     private Ray _ray;
-
+    /**
+     * this is the basic constructor for a ray:
+     * it receive ray and raduis
+     * @param radius
+     * @param ray
+     * @throws  if the radius equal on small to zero so we don't have a tube so his return IllegalArgumentException
+     */
     public Tube(Ray ray, double radius) {
         super(radius);
-        if (radius == 0)
+        if (radius <=0)
             throw new IllegalArgumentException("It's not possible to have radius equals to 0");
         this._ray = new Ray(ray);
     }
 
+    /**
+     * simple function get
+     * @return the ray
+     */
     public Ray getRay() {
         return _ray;
     }
 
+    /** the fucnction should return the normal of tube in size one
+     *
+     * @param point- he recive a point on cycle tube
+     * @param
+     * @return
+     */
     public Vector getNormal(Point3D point) {
         // distance from axis start to the projection of the point onto axis
         double scaleNumber = _ray.getDirection().dotProduct(point.subtract(_ray.getP0()));
-        // the projection of the point onto axis
+        // he find the point on the border of the tube
         Point3D o = _ray.getP0().add(_ray.getDirection().scale(scaleNumber));
+        // he rerun a new vector in size one
         return point.subtract(o).normalize();
     }
 
