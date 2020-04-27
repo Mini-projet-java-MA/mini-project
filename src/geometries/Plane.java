f63eb193431ec41d7fed012e1426d0a4d1afd72f
mghida <moche.ghidalia@gmail.com>
2020-04-27 14:53:57 +0300

    Geometries (Rec1 and Rec2) corrections + some test corrections Still: * to complete javadocs in geometries and in tests * missing comments in tests per requirements * code formatting, variable names, etc...

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -3,15 +3,31 @@ package geometries;
 import primitives.Point3D;
 import primitives.Vector;
 
+/**
+ * class plane represents a plane in 3D cartesian coordinate system
+ */
 public class Plane {
     private Point3D _p;
     private Vector _normal;
 
+    /**
+     * this is a basic constructor who buil a plane from 3 points int cartesian coordinates system
+     *
+     * @param _p1 1st point
+     * @param _p2 2nd point
+     * @param _p3 3rd point
+     */
     public Plane(Point3D _p1, Point3D _p2, Point3D _p3) {
         _p = new Point3D(_p1);
         _normal = getNormal(_p1, _p2, _p3);
     }
 
+    /**
+     * this constructor build a plane from a single point and a vector that will be normalized
+     *
+     * @param p      the point
+     * @param normal the vector normalized we'll use to build the plane
+     */
     public Plane(Point3D p, Vector normal) {
         _p = new Point3D(p);
         _normal = normal.normalized();
@@ -19,6 +35,7 @@ public class Plane {
 
     /**
      * Produce normal vector (orthogonal unit vector) from 3 points in the plane)
+     *
      * @param p1 1st point
      * @param p2 2nd point
      * @param p3 3rd point
@@ -34,10 +51,21 @@ public class Plane {
         return v1.crossProduct(v2).normalize();
     }
 
+    /**
+     * this function allows us to compute the normal vector in a specific point in the plane
+     *
+     * @param p the point where we want to compute the normal vector
+     * @return the normal vector computed in the specific point
+     */
     public Vector getNormal(Point3D p) {
         return _normal;
     }
 
+    /**
+     * this function returns the normal vector to the plane
+     *
+     * @return the vector normal to the plane
+     */
     public Vector getNormal() {
         return _normal;
     }
