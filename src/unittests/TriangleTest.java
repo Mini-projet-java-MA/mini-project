254c77cd6ae0211c02c8d054d95e40be7e2f97ee
mghida <moche.ghidalia@gmail.com>
2020-04-28 16:31:08 +0300

    Geometries (Rec1 and Rec2) corrections + some test corrections Still: * to complete javadocs in geometries and in tests * missing comments in tests per requirements * code formatting, variable names, etc...

--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -32,19 +32,21 @@ public class TriangleTest {
         Ray ray = new Ray(new Point3D(2, 5, 0), new Vector(0, 0, -1));
         Triangle triangle = new Triangle(new Point3D(0, 0, 0), new Point3D(7, 0, 0), new Point3D(0, 8, 0));
         List<Point3D> intersectionsList = triangle.findIntersections(ray);
-        assertNotNull("must be not empty", intersectionsList);
-        assertEquals("must be empty",1,intersectionsList.size());
+        assertNull("must be not empty", intersectionsList);
+        assertEquals("must be equal to 1",1,intersectionsList.size());
         assertEquals("must be the same",new Point3D(2.139,5.556,0),intersectionsList.get(0));
 
 
         //TC02 outside against edge
         ray = new Ray(new Point3D(5, 6, 0), new Vector(0, 0, -1));
         intersectionsList = triangle.findIntersections(ray);
+        assertNull("must be empty",intersectionsList);
 
 
         //TC02 outside against vertex
         ray = new Ray(new Point3D(-1, -1, 0), new Vector(0, 0, -1));
         intersectionsList = triangle.findIntersections(ray);
+        assertNull("must be empty",intersectionsList);
 
 
 
