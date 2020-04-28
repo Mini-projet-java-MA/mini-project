ede232d84166f34ac18d41aa3a86d51b8a7783e5
aaron <aaron@LAPTOP-JFVNBT5Q>
2020-04-28 10:44:05 +0300

    modiff raditiak geomery

--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -8,16 +8,23 @@ import java.util.List;
 import java.awt.Color;
 
 /**
- *
- */
+ *this is abstract class represent a shapes geometry that contain radius
+  */
 public abstract class RadialGeometry implements Geometry {
     protected double _radius;
-
+    /**
+     * this is simple constructor build the shapes witch radius
+     *
+     * @param radius
+     */
     public RadialGeometry(double radius) {
         _radius = radius;
     }
 
-
+    /**a simple func that return radius
+     *
+     * @return reduis
+     */
     public double getRadius() {
         return _radius;
     }
