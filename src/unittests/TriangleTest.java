eea191c387a3ce140f6c86dcfd423a907894315d
mghida <moche.ghidalia@gmail.com>
2020-04-28 14:57:23 +0300

    Geometries (Rec1 and Rec2) corrections + some test corrections Still: * to complete javadocs in geometries and in tests * missing comments in tests per requirements * code formatting, variable names, etc...

--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -1,4 +1,4 @@
-package unittests;
+ package unittests;
 
 import geometries.*;
 import org.junit.Test;
@@ -6,6 +6,7 @@ import primitives.*;
 
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.fail;
 
 
@@ -23,15 +24,28 @@ public class TriangleTest {
 
     @Test
     public void findIntersections() {
-        //TC01 on edge- start at the plane
-        Ray ray= new Ray(new Point3D(1, 0, 0), new Vector(0,0,-1));
-        Triangle triangle = new Triangle(new Point3D(0,0,0), new Point3D(7,0,0), new Point3D(0,8,0));
-        List<Point3D> intersectionsList= triangle.findIntersections(ray);
-        System.out.println(intersectionsList);
+        // ============ Equivalence Partitions Tests ==============
+        //TC01 inside triangle
+        Ray ray = new Ray(new Point3D(2, 5, 0), new Vector(0, 0, -1));
+        Triangle triangle = new Triangle(new Point3D(0, 0, 0), new Point3D(7, 0, 0), new Point3D(0, 8, 0));
+        List<Point3D> intersectionsList = triangle.findIntersections(ray);
+        assertArrayEquals(intersectionsList, );
+
+        //TC02 outside against edge
+        ray = new Ray(new Point3D(5, 6, 0), new Vector(0, 0, -1));
+        intersectionsList = triangle.findIntersections(ray);
+
+
+        //TC02 outside against vertex
+        ray = new Ray(new Point3D(-1, -1, 0), new Vector(0, 0, -1));
+        intersectionsList = triangle.findIntersections(ray);
 
 
 
-
+// =============== Boundary Values Tests ==================
+        //TC10 on edge- start at the plane
+        ray = new Ray(new Point3D(1, 0, 0), new Vector(0, 0, -1));
+        intersectionsList = triangle.findIntersections(ray);
 
 
 
