c78c627eca48f1bc01a071e9426b79ee86da73b7
mghida <moche.ghidalia@gmail.com>
2020-04-28 14:09:00 +0300

    Geometries (Rec1 and Rec2) corrections + some test corrections Still: * to complete javadocs in geometries and in tests * missing comments in tests per requirements * code formatting, variable names, etc...

--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -1,8 +1,10 @@
 package unittests;
 
-import geometries.Triangle;
+import geometries.*;
 import org.junit.Test;
-import primitives.Point3D;
+import primitives.*;
+
+import java.util.List;
 
 import static org.junit.Assert.fail;
 
@@ -17,5 +19,21 @@ public class TriangleTest {
             fail("Constructed a vector not a triangle ");
         } catch (IllegalArgumentException e) {
         }
+    }
+
+    @Test
+    public void findIntersections() {
+        //TC01 on edge- start at the plane
+        Ray ray= new Ray(new Point3D(1, 0, 0), new Vector(0,0,-1));
+        Triangle triangle = new Triangle(new Point3D(0,0,0), new Point3D(7,0,0), new Point3D(0,8,0));
+        List<Point3D> intersectionsList= triangle.findIntersections(ray);
+        System.out.println(intersectionsList);
+
+
+
+
+
+
+
     }
 }
