9e71a5815747c9038570fbcebc835b10de256d12
aaron <aaron@LAPTOP-JFVNBT5Q>
2020-05-14 15:46:18 +0300

    i dont know what chang

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -7,6 +7,12 @@ import primitives.Color;
  */
 public class AmbientLight {
     private Color _intensity;
+
+    /**
+     *the func refill light
+     * @param ia-refill light
+     * @param ka-promotes light refill
+     */
     public  AmbientLight(Color ia, double ka) {
         this._intensity = ia.scale(ka);
     }
