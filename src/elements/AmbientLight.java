ef2852abd4a0ebbeff5c0e204dc9541809277e40
aaron <aaron@LAPTOP-JFVNBT5Q>
2020-05-14 15:08:39 +0300

    test for img

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -3,10 +3,15 @@ package elements;
 import primitives.Color;
 
 /**
- * the class should represent
+ * the class should represent environment light
+ * @author aaron
  */
 public class AmbientLight {
-    Color _intensity;
+   private Color _intensity;
     public  AmbientLight(Color Ia, double Ka)
-    {}
+    {
+        // i dont 
+        _intensity=Ia;
+    }
+
 }
--- src/unittests/ImageWriterTest.java
+++ src/unittests/ImageWriterTest.java
@@ -1,32 +1,34 @@
 package unittests;
 
 import org.junit.Test;
+import primitives.Color;
+import renderer.ImageWriter;
 
-import static org.junit.Assert.*;
-
+/**
+ * in this class we test image writer
+ */
 public class ImageWriterTest {
 
-    @Test
-    public void getWidth() {
-    }
-
-    @Test
-    public void getHeight() {
-    }
-
-    @Test
-    public void getNy() {
-    }
-
-    @Test
-    public void getNx() {
-    }
-
+    /**
+     * we test to creat image 10*16 squared 1000*1600 unit light witch resolutions 500*800
+     */
     @Test
     public void writeToImage() {
-    }
+        Color Green = new Color(0, 0, 0);
+        Color white = new Color(255, 255, 255);
+        ImageWriter img = new ImageWriter("img1", 10, 16, 800, 500);
+        for (int col = 0; col< 500; col++) {
+            for (int row = 0; row < 800; row++) {
+                if (col % 50 == 0 || row % 50 == 0)
+                    img.writePixel(col, row, white.getColor());
+                else
+                    img.writePixel(row, row, Green.getColor());
 
-    @Test
-    public void writePixel() {
+            }
+            img.writeToImage();
+
+        }
+        img.writeToImage();
+
     }
 }
