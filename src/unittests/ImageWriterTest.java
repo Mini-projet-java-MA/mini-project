ec0d7c2304bb789332cd9288295bf7fc6e42a679
mghida <moche.ghidalia@gmail.com>
2020-05-14 14:15:23 +0300

    fix tests plane and reformatting variables

---
+++ src/unittests/ImageWriterTest.java
@@ -0,0 +1,32 @@
+package unittests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ImageWriterTest {
+
+    @Test
+    public void getWidth() {
+    }
+
+    @Test
+    public void getHeight() {
+    }
+
+    @Test
+    public void getNy() {
+    }
+
+    @Test
+    public void getNx() {
+    }
+
+    @Test
+    public void writeToImage() {
+    }
+
+    @Test
+    public void writePixel() {
+    }
+}
