a72bb95c6421682b5e428724789b911636c5e00c
aaron <aaron@LAPTOP-JFVNBT5Q>
2020-05-14 16:28:52 +0300

    4.1 modiff

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -1,9 +1,20 @@
 package renderer;
+import primitives.Color;
+import primitives.Point3D;
 import renderer.ImageWriter;
+import scene.Scene;
+
 /**
- *
+ *th object of this class his to creat matrix of picture
  */
 public class Render {
-    ImageWriter image;
-
+    private Scene _scene;
+    private ImageWriter _imageWriter;
+    /**
+     * @param point the point for which the color is required
+     * @return the color intensity
+     */
+    private Color calcColor(Point3D point) {
+        return _scene.getAmbientLight().getIntensity();
+    }
 }
