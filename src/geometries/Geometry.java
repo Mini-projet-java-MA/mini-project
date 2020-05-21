cd6ac03af2c6814f82e387a17fe00d6888b67e66
aaron <aaron@LAPTOP-JFVNBT5Q>
2020-05-21 15:50:43 +0300

    odiff

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,5 +1,6 @@
 package geometries;
 
+import primitives.Color;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
@@ -8,11 +9,13 @@ import java.util.List;
 /**
  * we do interface for geometry
  */
-public interface Geometry extends Intersectable {
+public abstract class Geometry implements Intersectable {
     /**
      * getNormal function to get the normal vector from a point on a geometry
      * @param point3D a point on geometry
      * @return the normal vector
      */
+    protected Color _emission;
+
     Vector getNormal(Point3D point3D);
 }
